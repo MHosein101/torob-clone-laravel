652cf9fa004e738a038f6d0669434253963c8192
MHosein101 <pm101255@gmail.com>
2022-09-03 11:47:24 +0430

    add seeder data

--- database/seeders/OfferSeeder.php
+++ database/seeders/OfferSeeder.php
@@ -28,6 +28,35 @@ class OfferSeeder extends Seeder
 
         Offer::create([ 'product_id' => 1 , 'shop_id' => 3 , 'is_available' => true , 'price' => 9000 ]);
 
+
+        Offer::create([ 'product_id' => 5 , 'shop_id' => 1 , 'is_available' => true , 'price' => 3000 ]);
+        Offer::create([ 'product_id' => 5 , 'shop_id' => 2 , 'is_available' => true , 'price' => 3200 ]);
+        Offer::create([ 'product_id' => 5 , 'shop_id' => 3 , 'is_available' => true , 'price' => 2900 ]);
+
+        Offer::create([ 'product_id' => 6 , 'shop_id' => 1 , 'is_available' => true , 'price' => 2500 ]);
+        Offer::create([ 'product_id' => 6 , 'shop_id' => 2 , 'is_available' => true , 'price' => 1900 ]);
+        Offer::create([ 'product_id' => 6 , 'shop_id' => 3 , 'is_available' => true , 'price' => 1900 ]);
+
+        Offer::create([ 'product_id' => 7 , 'shop_id' => 1 , 'is_available' => true , 'price' => 4000 ]);
+        Offer::create([ 'product_id' => 7 , 'shop_id' => 3 , 'is_available' => true , 'price' => 3900 ]);
+
+        Offer::create([ 'product_id' => 8 , 'shop_id' => 1 , 'is_available' => true , 'price' => 15000 ]);
+        Offer::create([ 'product_id' => 8 , 'shop_id' => 2 , 'is_available' => true , 'price' => 11000 ]);
+        Offer::create([ 'product_id' => 8 , 'shop_id' => 3 , 'is_available' => true , 'price' => 17000 ]);
+
+        Offer::create([ 'product_id' => 9 , 'shop_id' => 1 , 'is_available' => true , 'price' => 21000 ]);
+        Offer::create([ 'product_id' => 9 , 'shop_id' => 2 , 'is_available' => true , 'price' => 22000 ]);
+        Offer::create([ 'product_id' => 9 , 'shop_id' => 3 , 'is_available' => true , 'price' => 19000 ]);
+
+        Offer::create([ 'product_id' => 10 , 'shop_id' => 1 , 'is_available' => true , 'price' => 130 ]);
+        Offer::create([ 'product_id' => 10 , 'shop_id' => 2 , 'is_available' => true , 'price' => 150 ]);
+        Offer::create([ 'product_id' => 10 , 'shop_id' => 3 , 'is_available' => true , 'price' => 200 ]);
+
+        Offer::create([ 'product_id' => 11 , 'shop_id' => 1 , 'is_available' => true , 'price' => 90 ]);
+        Offer::create([ 'product_id' => 11 , 'shop_id' => 2 , 'is_available' => true , 'price' => 120 ]);
+        Offer::create([ 'product_id' => 11 , 'shop_id' => 3 , 'is_available' => true , 'price' => 100 ]);
+
+
         // Offer::create([ 'product_id' => 1 , 'shop_id' => 1 , 'is_available' => true , 'price' => 1000 ]);
     }
 }
